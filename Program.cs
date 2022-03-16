f124a2162a4578f4f4fc3bf1ad7755f399bf97f9
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2022-03-16 14:42:16 -0600

    Bug fixes and New packages

--- Program.cs
+++ Program.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Linq;
 
 namespace SeleniumFirst
 {
     class Program
     {
-        static async void Main(string[] args)
+        static void Main(string[] args)
         {
             //Create tests names
             var tests = new List<string>() { "Ideal", "Nothing","Missing Required Name","Missing Required ZipCode",
@@ -19,47 +18,51 @@ namespace SeleniumFirst
                     "CardNumber","ExpirationMonth","ExpirationYear","CVV"};
 
             var values = new List<List<string>>();
-            values.Add(new List<string> { "John Doe","john@doe.com", "123 S 4095 W","Some City", "Somewhere", "84042", "4444-1234-1234-1234", "August", "24", "685" });
-            values.Add(new List<string> { "","", "", "", "", "", "", "", "" });
-            values.Add(new List<string> { "", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "4444-1234-1234-1234", "August", "24", "685" });
-            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "", "4444-1234-1234-1234", "August", "24", "685" });
-            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "", "", "", "" });
-            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "4444-1234-1234-12", "August", "24", "685" });
-            values.Add(new List<string> { "<svg onload=alert(1)><svg>", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "4444-1234-1234-1234", "August", "24", "685" });
+            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042","John Doe", "4444-1234-1234-1234", "August", "24", "685" });
+            values.Add(new List<string> { "", "", "", "", "", "", "", "", "","","" });
+            values.Add(new List<string> { "", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "John Doe", "4444-1234-1234-1234", "August", "24", "685" });
+            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "", "John Doe", "4444-1234-1234-1234", "August", "24", "685" });
+            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "John Doe", "", "", "", "" });
+            values.Add(new List<string> { "John Doe", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "John Doe", "4444-1234-1234-12", "August", "24", "685" });
+            values.Add(new List<string> { "<svg onload=alert(1)><svg>", "john@doe.com", "123 S 4095 W", "Some City", "Somewhere", "84042", "John Doe", "4444-1234-1234-1234", "August", "24", "685" });
 
-            //Create the reference for our browser
-            IWebDriver driver = new ChromeDriver();
-
-            foreach (var t in tests)
-            {
-                foreach(var v in values)
+                int t = 0;
+                foreach (var v in values)
                 {
+                    //Create the reference for our browser
+                    IWebDriver driver = new ChromeDriver();
                     //navigate to Program page
                     driver.Navigate().GoToUrl("https://localhost:5001/");
-                    IWebElement addtocart = driver.FindElement(By.Value("CfDJ8N97rxC2o9RAkJZ_0i3wOmY7LyjlMtJMquXFz5YEbaQmgldK_cESFgpoC2xuU-HY_ZU74I1kBXkxK_DazX6XGKZsaizhnH4XFT9KtspuQi46ZGCBwzu04jqkUsmN4wCIkBcXKAD5QHxgurZPrMRWLTQ"));
-                    addtocart.click();
+                    IWebElement addtocart = driver.FindElement(By.Name("AddToCart"));
+                    addtocart.Click();
                     IWebElement checkout = driver.FindElement(By.Name("checkout"));
-                    checkout.click();
+                    checkout.Click();
                     int i = 0;
                     foreach (var tgn in tagNames)
                     {
-                            // Identify value
-                            var val = v[i];
-                            // Do Something
-                            element.SendKeys(val);
-                            i++;  
+                        IWebElement element = driver.FindElement(By.Name(tgn));
+                        // Identify value
+                        var val = v[i];
+                        // Do Something
+                        element.SendKeys(val);
+                        i++;
                     }
-                    IWebElement submitorder = driver.FindElement(By.Class("MYbtn"));
-                    try{
-                            submitorder.click();
-                            Console.WriteLine(t,"Success");
-                        }
-                    catch (Exception e){
-                            Console.WriteLine(t, "Failure");
-                        }
-                    
+                    IWebElement submitorder = driver.FindElement(By.ClassName("MYbtn"));
+                    try
+                    {
+                        submitorder.Click();
+                        Console.WriteLine(tests[t]);
+                        Console.WriteLine("Success");
+                        driver.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(tests[t]);
+                        Console.WriteLine("Failure");
+                        driver.Close();
+                    }
+                t++;
                 }
-            }
         }
     }
 }
